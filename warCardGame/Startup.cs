48a5c9fd7687d43d1290e06b652b302739d2fbb3
unknown <Chandler Claxton>
2016-06-17 20:47:29 -0400

    Initial push of default asp.net project

---
+++ warCardGame/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(warCardGame.Startup))]
+namespace warCardGame
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
